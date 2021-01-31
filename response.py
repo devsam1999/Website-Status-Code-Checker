99f5655534f553f6ecb900d3523d849f592da803
Sahil Mishra <78307289+devsam1999@users.noreply.github.com>
2021-01-31 19:58:04 +0530

    Update response.py

--- response.py
+++ response.py
@@ -1,3 +1,6 @@
+#This Program will help u to check the existance of bulk urls.
+#Author Chittaranjan Kumar (@chitta2019) and Sahil Mishra (@devsam1999)
+#Just_for_fun
 import requests
 url_file = open('web.txt', 'r')#replace your file name with web.txt
 url=url_file.read()
