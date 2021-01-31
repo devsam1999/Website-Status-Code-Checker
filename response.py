b1b7e9e02cbc80d21d987f76b48d25c8a87b3a8e
Sahil Mishra <78307289+devsam1999@users.noreply.github.com>
2021-01-31 19:40:37 +0530

    Update response.py

--- response.py
+++ response.py
@@ -1,8 +1,7 @@
 import requests
-url_file = open('web.txt', 'r')
+url_file = open('web.txt', 'r')#replace your file name with web.txt
 url=url_file.read()
 newurl= url.splitlines()
-#print (newurl)
 for x in newurl:
 	print(x)
 	response = requests.get(x)
